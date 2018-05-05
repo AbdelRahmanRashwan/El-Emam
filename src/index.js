585d2103a0c505ba88d6af1a9cd236ca4ff5f737
A-Hassan-M <mido20140014@gmail.com>
2018-05-05 22:54:36 +0200

    header background

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Bio from './components/Bio/Bio';
+import Media from './components/Home/Media';
 import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<Bio />, document.getElementById('root'));
 registerServiceWorker();
