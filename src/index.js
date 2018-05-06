8495089769eb36bfdebae4449e2c72626eca8580
Rashwan <abdo.ra.1997@gmail.com>
2018-05-05 22:46:35 -0700

    navbar is navigating and routes are established

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Media from './components/Home/Media';
-import registerServiceWorker from './registerServiceWorker';
-ReactDOM.render(<Media />, document.getElementById('root'));
-registerServiceWorker();
+import Bio from './components/Bio/Bio';
+
+class App extends React.Component{
+    
+    render(){
+        return (
+            <BrowserRouter > 
+                <div>
+                    <Route path = "/Bio" component = {Bio}/>
+                    <Route path = "/Media" component = {Media}/>
+                    <Route path = "/News" component = {Media}/>
+                    <Route path = "/Mo2alfat" component = {Bio}/>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
